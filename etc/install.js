33e5deb6ddf4fab13e451144814957cfcb3487ab
cburkey <cburkey@entermediasoftware.com>
2011-07-18 09:42:47 -0400

    Michael Tweak

---
+++ etc/install.js
@@ -0,0 +1,30 @@
+importPackage( Packages.com.openedit.util );
+importPackage( Packages.java.util );
+importPackage( Packages.java.lang );
+importPackage( Packages.com.openedit.modules.update );
+
+var appname = "medialab";
+
+var zip = "http://dev.entermediasoftware.com/jenkins/job/app-" + appname + "/lastSuccessfulBuild/artifact/deploy/app-" + appname + ".zip";
+
+var root = moduleManager.getBean("root").getAbsolutePath();
+var eml = root + "/"  + appname;
+var tmp = root + "/WEB-INF/tmp";
+
+log.add("1. GET THE LATEST ZIP FILE");
+var downloader = new Downloader();
+downloader.download( war, tmp + "/app.zip");
+
+log.add("2. UNZIP WAR FILE");
+var unziper = new ZipUtil();
+unziper.unzip(  tmp + "/app.zip",  tmp );
+
+var files = new FileUtils();
+log.add("4. UPGRADE BASE DIR");
+files.deleteAll( root + "/" + appname);
+files.copyFiles( tmp + "/" + appname, root);
+
+log.add("5. CLEAN UP");
+files.deleteAll(tmp);
+
+log.add("6. UPGRADE COMPLETED");
